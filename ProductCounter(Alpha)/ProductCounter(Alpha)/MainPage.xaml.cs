48f3626f871c8d14b5e5e8d06dc05181a10ef947
Богдан Черный <booggiman@mail.ru>
2022-11-26 15:38:02 +0200

    Scanner added to application

--- ProductCounter(Alpha)/ProductCounter(Alpha)/MainPage.xaml.cs
+++ ProductCounter(Alpha)/ProductCounter(Alpha)/MainPage.xaml.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZXing;
 
 namespace ProductCounter_Alpha_
 {
     public partial class MainPage : ContentPage
     {
+        private string _link;
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void ZXingScannerViev_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                scanResultText.Text = Convert.ToString(result);
+            });
+        }
     }
 }
+
